aa05cdff0c4032fa34366ab28e9558e104189b78
programmermohaimin <you@example.com>
2022-08-04 15:41:53 +0600

    all the functionality is working gooooood

--- app.js
+++ app.js
@@ -1,26 +1,212 @@
 
 
-let debug = document.getElementById('debug');
-let lea = document.getElementById('lea');
+
+let oneBun = document.getElementById('oneBun');
+let listOne = document.getElementById('listOne');
+let nam = document.getElementById('name');
+
+let listOne2 = document.getElementById('listOne2');
+let nam2 = document.getElementById('name2');
+let oneBun2 = document.getElementById('oneBun2');
 
 
-fetch("config.json")
+
+
+// hamburger menu
+oneBun.addEventListener('click', function () {
+    
+    if (listOne.style.display === 'block') {
+        listOne.style.display = 'none'
+
+    } else if (listOne.style.display = 'none') {
+        listOne.style.display = 'block'
+    }
+})
+
+
+// change header text 
+let child = listOne.children;
+[...child].forEach(children => {
+    children.addEventListener('click', function () {
+        nam.innerHTML = children.innerHTML
+        
+        listOne.style.display = 'none'
+    })
+})
+
+
+
+// second
+// hamburger menu
+oneBun2.addEventListener('click', function () {
+    
+    if (listOne2.style.display === 'block') {
+        listOne2.style.display = 'none'
+
+    } else if (listOne2.style.display = 'none') {
+        listOne2.style.display = 'block'
+    }
+})
+
+
+// change header text 
+let chi = listOne2.children;
+[...chi].forEach(children => {
+    children.addEventListener('click', function () {
+        nam2.innerHTML = children.innerHTML
+        
+        listOne2.style.display = 'none'
+    })
+})
+
+
+
+
+
+
+
+
+
+let productName = document.getElementById('productName');
+let size = document.getElementById('size');
+let brand = document.getElementById('brand');
+let ram = document.getElementById('ram');
+
+
+
+
+let nokiaID = document.getElementById('nokia');
+let motorolaID = document.getElementById('motorola');
+let iphoneID = document.getElementById('iphone');
+let samsungID = document.getElementById('samsung');
+let vivoID = document.getElementById('vivo');
+
+
+
+
+// Nokia--------->
+window.onload = () => {
+    nokiaID.addEventListener('click', function () {
+        nokia();
+    })
+
+    motorolaID.addEventListener('click', function () {
+        motorola();
+    })
+    iphoneID.addEventListener('click', function () {
+        iphone();
+    })
+    samsungID.addEventListener('click', function () {
+        samsung();
+    })
+    vivoID.addEventListener('click', function () {
+        vivo();
+    })
+
+
+
+
+
+}
+
+
+
+function nokia() {
+    fetch("config.json")
     .then(response => response.json())
     .then(data => {
         
-        data.productOne.forEach(list => {
-            debug.addEventListener("click", function () {
-                document.getElementById('debug').innerHTML = list.name
-            })
-            lea.addEventListener('click', function () {
-            document.getElementById('lea').innerHTML = list.brand
-                
-            })
+        data.nokia.forEach(list => {
+            productName.innerHTML = list.name
+            size.innerHTML =list.size
+            brand.innerHTML =list.brand
+            ram.innerHTML =list.ram
             
         })
-
-        data.productTwo.forEach(list => {
-            console.log(list);
-        })
-        
     })
+}
+
+
+
+
+
+// Motorola --------->
+
+function motorola() {
+    fetch("config.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        data.motorola.forEach(list => {
+            productName.innerHTML = list.name
+            size.innerHTML =list.size
+            brand.innerHTML =list.brand
+            ram.innerHTML =list.ram
+            
+        })
+    })
+}
+
+
+
+
+
+
+// I phone --------->
+
+function iphone() {
+    fetch("config.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        data.iphone.forEach(list => {
+            productName.innerHTML = list.name
+            size.innerHTML =list.size
+            brand.innerHTML =list.brand
+            ram.innerHTML =list.ram
+            
+        })
+    })
+}
+
+
+
+
+
+// Samsung --------->
+
+function samsung() {
+    fetch("config.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        data.samsung.forEach(list => {
+            productName.innerHTML = list.name
+            size.innerHTML =list.size
+            brand.innerHTML =list.brand
+            ram.innerHTML =list.ram
+            
+        })
+    })
+}
+
+
+
+// ViVo --------->
+
+function vivo() {
+    fetch("config.json")
+    .then(response => response.json())
+    .then(data => {
+        
+        data.vivo.forEach(list => {
+            productName.innerHTML = list.name
+            size.innerHTML =list.size
+            brand.innerHTML =list.brand
+            ram.innerHTML =list.ram
+            
+        })
+    })
+}
+
+
